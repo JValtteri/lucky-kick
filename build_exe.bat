cd4e5d8b05f9e61ed05d8d2d424818c1b1f0b1c0
JValtteri <46611993+JValtteri@users.noreply.github.com>
2020-10-07 22:53:12 +0300

    First executible build

--- build_exe.bat
+++ build_exe.bat
@@ -4,9 +4,9 @@ cd lc-venv\Scripts
 call activate.bat
 cd ../..
 @echo Build EXE
-pyinstaller game.py --noconsole
-:: @echo Move exe to root
-:: move /Y dist\game.exe game.exe
+pyinstaller game.py --onefile --noconsole
+@echo Move exe to root
+move /Y dist\game.exe game.exe
 @echo Exit Virtual Enviroment
 deactivate
 pause
